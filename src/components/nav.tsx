4067d939c98135495557a37bac044b321079b489
na0nie <naonchun@gmail.com>
2023-07-12 12:13:23 -0700

    clean up

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,39 +1,5 @@
 import { Disclosure } from "@headlessui/react";
 import { ChevronDoubleDownIcon, XIcon } from "@heroicons/react/outline";
-import ActiveLink from "./ActiveLink";
-import { MapTitle } from "./MapTitle";
-
-// const navigation = [
-//   {
-//     name: "2019",
-//     url: "https://2019arrests.lacontroller.io/",
-//     newtab: true,
-//   },
-//   {
-//     name: "2020",
-//     url: "https://2020arrests.lacontroller.io/",
-//     newtab: true,
-//   },
-//   {
-//     name: "2021",
-//     url: "https://2021arrests.lacontroller.io/",
-//     newtab: true,
-//   },
-//   {
-//     name: "2022",
-//     url: "/",
-//   },
-//   {
-//     name: "Table",
-//     url: "https://docs.google.com/spreadsheets/d/1rH272RxRxU8ut3xArEnyOz2j_Qlw5ntEoTJlZwcx_Ng/edit?usp=sharing",
-//     newtab: true,
-//   },
-//   {
-//     name: "LA Controller",
-//     url: "https://controller.lacity.gov",
-//     newtab: true,
-//   },
-// ];
 
 interface newiteminterface {
   name: string;
@@ -109,22 +75,6 @@ function Nav() {
   };
 
   return (
-    // <div className="z-50 bg-[#1a1a1a] flex flex-col">
-    //   <nav className="z-50 flex flex-row  h-content">
-    //     {navigationPayroll.map((item: any, itemIdx: any) => (
-    //       <ActiveLink
-    //         activeClassName="text-white py-2 md:py-3 px-3 block hover:text-green-300 focus:outline-none text-green-300 border-b-2 font-medium border-green-300"
-    //         href={item.url}
-    //         key={itemIdx}
-    //         target={`${item.newtab === true ? "_blank" : ""}`}
-    //       >
-    //         <p className="text-white py-2 text-sm md:text-base md:py-3 px-3 block hover:text-green-300 focus:outline-none underline">
-    //           {item.name}
-    //         </p>
-    //       </ActiveLink>
-    //     ))}
-    //   </nav>
-    // </div>
     <Disclosure as="nav" className="z-50 bg-[#1a1a1a] flex flex-col">
       {({ open }) => (
         <>
